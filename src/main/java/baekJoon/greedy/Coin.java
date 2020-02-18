57764fef2bb48ee5f3d672cd08ef5b0a87398504
LimHanGyeol <dlagksruf19@naver.com>
2020-02-18 17:39:55 +0900

    백준 - (그리디) 동전 풀이

---
+++ src/main/java/baekJoon/greedy/Coin.java
@@ -0,0 +1,46 @@
+package baekJoon.greedy;
+
+import java.util.Scanner;
+
+/**
+ * 2020. 02. 18.
+ * 문제 : 11047번 동전
+ */
+public class Coin {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int K = scanner.nextInt();
+        int[] coinArray = new int[N + 1];
+
+        int minValue = 0;
+
+        for (int i = 0; i < N; i++) {
+            coinArray[i] = scanner.nextInt();
+        }
+
+        for (int i = coinArray.length - 1; i >= 0; i--) {
+            if (K > coinArray[i] && coinArray[i] != 0) {
+                minValue += K / coinArray[i];
+                K %= coinArray[i];
+            }
+        }
+        System.out.println(minValue);
+    }
+
+    public int getCoin(int k, int[] coinArray) {
+        int minValue = 0;
+        int price = k;
+
+        for (int i = coinArray.length - 1; i >= 0; i--) {
+            if (price == 0) break;
+            if (price > coinArray[i]) {
+                minValue += price / coinArray[i];
+                price = price % coinArray[i];
+            }
+        }
+        System.out.println(minValue);
+        return minValue;
+    }
+}
---
+++ src/test/java/baekJoonTest/greedytest/GreedyTest.java
@@ -0,0 +1,24 @@
+package baekJoontest.greedytest;
+
+import baekJoon.greedy.Coin;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class GreedyTest {
+
+    private Coin coin;
+
+    @BeforeEach
+    void setUp() {
+        coin = new Coin();
+    }
+
+    @Test
+    public void getCoinTest() {
+        assertThat(coin.getCoin(4200, new int[]{1, 5, 10, 50, 100, 500, 1000, 5000, 10000, 50000}), is(6));
+        assertThat(coin.getCoin(4790, new int[]{1, 5, 10, 50, 100, 500, 1000, 5000, 10000, 50000}), is(12));
+    }
+}
