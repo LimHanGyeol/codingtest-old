fa15949bb114624841ffaf332712fcd9cb626b19
LimHanGyeol <dlagksruf19@naver.com>
2020-04-22 01:24:14 +0900

    공부 - 정렬 알고리즘 (버블 정렬)

--- src/main/java/sort/BubbleSort.java
+++ src/main/java/sort/BubbleSort.java
@@ -7,11 +7,42 @@ public class BubbleSort {
     private static int[] input = {5, 6, 2, 8, 7, 23, 4, 1};
 
     public static void main(String[] args) {
-        bubbleSort(input, input.length);
-        for (int num : input) {
-            System.out.printf(num + " ");
-        }
+//        bubbleSort(input, input.length);
+//        for (int num : input) {
+//            System.out.printf(num + " ");
+//        }
 
+        printArray(input);
+        bubbleSortR(input);
+        printArray(input);
+    }
+
+    private static void bubbleSortR(int[] arr) {
+        bubbleSortR(arr, arr.length - 1);
+    }
+
+    private static void bubbleSortR(int[] arr, int last) {
+        if (last > 0) {
+            for (int i = 1; i <= last; i++) {
+                if (arr[i-1] > arr[i]) {
+                    swap(arr, i - 1, i);
+                }
+            }
+            bubbleSortR(arr, last - 1);
+        }
+    }
+
+    private static void swap(int[] arr, int source, int target) {
+        int tempValue = arr[source];
+        arr[source] = arr[target];
+        arr[target] = tempValue;
+    }
+
+    private static void printArray(int[] arr) {
+        for (int data : arr) {
+            System.out.print(data + ", ");
+        }
+        System.out.println();
     }
 
     private static void bubbleSort(int[] input, int length) {
